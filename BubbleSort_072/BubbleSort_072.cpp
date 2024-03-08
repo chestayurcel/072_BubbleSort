f89f8c08f86ec46c550f6ac99e1e155f2afba182
Yurcell <chestayurcel@gmail.com>
2024-03-08 16:35:12 +0700

    Add project files.

---
+++ BubbleSort_072/BubbleSort_072.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int arr[20]; // Deklarasi variabel global array a dengan ukuran 20
+int n; // Deklarasi variabel global n untuk menyimpan banyak elemen array
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
