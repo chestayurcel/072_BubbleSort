c42b1afe649331e4e9c36ed76686f8a2353cde8c
Yurcell <chestayurcel@gmail.com>
2024-03-08 16:36:32 +0700

    menambahkan int arr[20] dan int n (deklarasi variabel global)

--- BubbleSort_072/BubbleSort_072.cpp
+++ BubbleSort_072/BubbleSort_072.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int arr[20]; // Deklarasi variabel global array a dengan ukuran 20
 int n; // Deklarasi variabel global n untuk menyimpan banyak elemen array
 
+
 int main()
 {
     std::cout << "Hello World!\n";
