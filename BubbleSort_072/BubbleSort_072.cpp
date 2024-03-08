e341ebfd64e959c319421e58ac7a8cb5c52cacd8
Yurcell <chestayurcel@gmail.com>
2024-03-08 16:59:53 +0700

    step 1, 2, 3, 4 dan 5 dalam pembuatan prosedur untuk void bubble sort array

--- BubbleSort_072/BubbleSort_072.cpp
+++ BubbleSort_072/BubbleSort_072.cpp
@@ -26,7 +26,38 @@ void input() {  // prosedur untuk input
         cin >> arr[i]; // output dari pengguna
     }
 }
+void bubblesortArray() { // prosedur untuk mengurutkan array dengan metode bubble sort
+    int pass = 1; // step 1
 
+    do {
+        for (int j = 0; j <= n - 1 - pass; j++) { // step 2
+           if (arr[j] > arr[j + 1]) { // step 3
+               int temp;
+               temp = arr[j];
+               arr[j] = arr[j + 1];
+               arr[j + 1] = temp;
+           }
+        }
+        pass = pass + 1; // step 4
+
+        cout << "\nPass " << pass - 1 << ": "; // number of pass
+        for (int k = 0; k < n; k++) {
+            cout << arr[k] << "";
+        }
+        cout << endl;
+    } while (pass <= n - 1); // step 5
+
+
+
+
+
+
+
+
+
+
+
+}
 int main()
 {
     std::cout << "Hello World!\n";
