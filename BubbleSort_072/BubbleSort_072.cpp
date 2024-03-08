92e270ca2b17b70f8df32e947e427ec17ea791cd
Yurcell <chestayurcel@gmail.com>
2024-03-08 17:08:12 +0700

    menambahkan void display untuk menampilkan array dan jumlah pass

--- BubbleSort_072/BubbleSort_072.cpp
+++ BubbleSort_072/BubbleSort_072.cpp
@@ -46,17 +46,24 @@ void bubblesortArray() { // prosedur untuk mengurutkan array dengan metode bubbl
         }
         cout << endl;
     } while (pass <= n - 1); // step 5
-
-
-
-
-
-
-
-
-
-
-
+}
+void display() {
+    cout << endl;
+    cout << "=================================" << endl;
+    cout << "Element Array yang telah tersusun" << endl;
+    cout << "=================================" << endl;
+    cout << endl;
+    for (int j = 0; j < n; j++) {
+        cout << arr[j]; // menampilkan array
+        if (j < n - 1) {
+            cout << " ---> ";
+        }
+    }
+    cout << endl;
+    cout << endl;
+    cout << " Jumlah Pass = " << n - 1 << endl; // menampilkan jumlah pass
+    cout << endl;
+    cout << endl;
 }
 int main()
 {
